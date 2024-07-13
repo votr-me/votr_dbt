5c3fcdb1056bc9c053d6e276a80407252ec4c277
Tom Curran <tcurran4589@gmail.com>
2024-07-13 00:54:46 -0400

    created legislation activity table and views

---
+++ votr_dbt/models/congress/fact/views/congress_member_legislation_activity_latest.sql
@@ -0,0 +1,13 @@
+
+{{ config(
+    materialized='view'
+) }}
+
+with current_members as (
+    select *
+    from {{ ref('congress_member_legislation_activity_historical') }}
+    where is_current_member
+)
+
+select * 
+from current_members
