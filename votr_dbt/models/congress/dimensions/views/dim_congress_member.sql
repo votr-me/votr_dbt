e11bf4d2e96e1a00031bb45508f4c10580de790b
Tom Curran <tcurran4589@gmail.com>
2024-07-13 10:48:17 -0400

    Update dim_congress_member.sql

--- votr_dbt/models/congress/dimensions/views/dim_congress_member.sql
+++ votr_dbt/models/congress/dimensions/views/dim_congress_member.sql
@@ -7,6 +7,16 @@ with current_members as (
     select *
     from {{ ref('dim_congress_member_historical') }}
     where is_current_member
+),
+
+leadership_roles as (
+    select *
+    from {{ ref('dim_congress_member_leadership_historical') }}
+    where is_current
 )
 
-select * from current_members
+select
+    current_members.*,
+    leadership_roles.leadership_type
+from current_members left join leadership_roles
+    on current_members.bioguide_id = leadership_roles.bioguide_id
