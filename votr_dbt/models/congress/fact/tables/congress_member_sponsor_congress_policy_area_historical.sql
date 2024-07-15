26ddf6c34cf04188eacb1ec661155cbd9ed555f0
Tom Curran <tcurran4589@gmail.com>
2024-07-15 17:21:22 -0400

    sponsors_tables

---
+++ votr_dbt/models/congress/fact/tables/congress_member_sponsor_congress_policy_area_historical.sql
@@ -0,0 +1,26 @@
+{{ config(
+    materialized='table'
+) }}
+
+with bill_sponsors as (
+    select *
+    from {{ ref('dim_bill_sponsors_historical') }}
+),
+bill_info as (
+    select *
+    from {{ ref('dim_bill_info_historical') }}
+)
+select
+    a.bioguide_id,
+    b.congress,
+    b.policy_area_name,
+    count(distinct b.bill_id) as num_bills_sponsored_bills
+from
+    bill_sponsors a
+left join
+    bill_info b
+    on a.bill_id = b.bill_id
+group by
+    a.bioguide_id,
+    b.congress,
+    b.policy_area_name
