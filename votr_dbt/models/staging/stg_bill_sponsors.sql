18c4b67980b8b798b54094a48643924e59b07124
Tom Curran <tcurran4589@gmail.com>
2024-07-15 17:21:22 -0400

    sponsors_tables

--- votr_dbt/models/staging/stg_bill_sponsors.sql
+++ votr_dbt/models/staging/stg_bill_sponsors.sql
@@ -1,9 +1,5 @@
 SELECT
-    "bioguideId" AS bioguide_id,
-    "bill_id" AS bill_id,
-    CASE
-        WHEN "isByRequest" = 'N' THEN FALSE
-        WHEN "isByRequest" = 'Y' THEN TRUE
-        ELSE NULL
-    END AS is_by_request
-from {{ source('raw', 'bill_sponsors') }}
+    bioguide_id,
+    bill_id,
+    is_by_request
+from {{ source('raw', 'bill_sponsors_2') }}
