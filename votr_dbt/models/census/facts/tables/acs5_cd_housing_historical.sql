835344d5658c27ca4ca11e984fc3875f9644f5f2
Tom Curran <tcurran4589@gmail.com>
2024-07-30 11:12:03 -0400

    added acs views

---
+++ votr_dbt/models/census/facts/tables/acs5_cd_housing_historical.sql
@@ -0,0 +1,21 @@
+{{ config(
+    materialized='table'
+) }}
+
+SELECT
+    year,
+    state_fip, 
+    congressional_district,
+    tenure_total,
+    tenure_total_moe,
+    owner_occupied,
+    owner_occupied_moe,
+    renter_occupied,
+    renter_occupied_moe,
+    median_contract_rent,
+    median_contract_rent_moe,
+    rent_asked_total,
+    rent_asked_total_moe,
+    median_gross_rent,
+    median_gross_rent_moe
+FROM {{ ref('stg_acs5_congressional_districts') }}
