85a98ed6e349494ccf16a9515510d20b178a0ba0
Tom Curran <tcurran4589@gmail.com>
2024-08-04 22:53:32 -0400

    added primary key to acs5 table

--- votr_dbt/models/congress/dimensions/views/dim_congress_member.sql
+++ votr_dbt/models/congress/dimensions/views/dim_congress_member.sql
@@ -3,20 +3,6 @@
     materialized='view'
 ) }}
 
-with current_members as (
     select *
     from {{ ref('dim_congress_member_historical') }}
     where is_current_member
-),
-
-leadership_roles as (
-    select *
-    from {{ ref('dim_congress_member_leadership_historical') }}
-    where is_current
-)
-
-select
-    current_members.*,
-    leadership_roles.leadership_type
-from current_members left join leadership_roles
-    on current_members.bioguide_id = leadership_roles.bioguide_id
